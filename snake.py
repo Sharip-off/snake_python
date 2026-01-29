d89ffe1c1911745c1135aeb437b0e136b7611ac7
Valerii Sharipov <tixan84@gmail.com>
2026-01-29 18:32:03 +0200

    the end of day 20

--- snake.py
+++ snake.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -8,6 +12,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
     
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -22,20 +27,20 @@ class Snake:
             new_x = self.segments[seg_num-1].xcor()
             new_y = self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.segments[0].heading()!= 270:
-            self.segments[0].setheading(90)
+        if self.head.heading()!= DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        if self.segments[0].heading() != 90:
-            self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def right(self):
-        if self.segments[0].heading()!=180:
-            self.segments[0].setheading(0)
+        if self.head.heading()!=LEFT:
+            self.head.setheading(RIGHT)
     
     def left(self):
-        if self.segments[0].heading()!=0:
-            self.segments[0].setheading(180)
+        if self.head.heading()!=RIGHT:
+            self.head.setheading(LEFT)
